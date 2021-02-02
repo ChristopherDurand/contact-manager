9de13824b7e0be1a1bf202cedf7734beee82edc3
ChristopherDurand <fonsuder@gmail.com>
2021-02-01 18:51:03 -0600

    Slight tidying

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const path = require('path');
 const contactRouter = require('./lib/contact_router');
 const app = express();
